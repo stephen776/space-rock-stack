a99df6c14b9f74c04e1e0d6f4e69f9f5f1a59ed5
Steve Jacobs <sjacobs@truefit.io>
2022-11-29 16:10:43 -0500

    add example of querying prisma to default route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,31 +1,27 @@
+import type {LoaderArgs} from '@remix-run/node';
+import {typedjson, useTypedLoaderData} from 'remix-typedjson';
+
+import {db} from '~/db.server';
+
+export async function loader({request}: LoaderArgs) {
+  const users = await db.user.findMany();
+
+  return typedjson({users});
+}
+
 export default function Index() {
+  const {users} = useTypedLoaderData<typeof loader>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div>
       <h1>Welcome to Remix</h1>
+
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {users.map((u) => (
+          <li key={u.email}>
+            {u.name} - {u.email}
+          </li>
+        ))}
       </ul>
     </div>
   );
